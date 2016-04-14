09fa4ac965f524f4e79fff6e1d7b401c98415c0c
zlevitt <zlevitt@users.noreply.github.com>
2016-04-14 16:46:38 -0400

    Added files via upload

---
+++ part_1.ino
@@ -0,0 +1,39 @@
+//CONSTANTS!!!
+const int numMotors = 3;
+
+//END OF CONSTANTS
+
+boolean readyForVal = false;
+int motorForces[numMotors];
+int motorIndex;
+
+void loop() {
+  int incomingByte;
+
+  if(Serial.available()>0)
+  {
+    incomingByte = Serial.read();
+    evaluateByte(incomingByte);
+  }
+}
+
+evaluateByte(int theByte) {
+  if (readyForVal) {
+    motorForces[motorIndex] = map(theByte, 0, 127, 1100, 1900);
+    readyForVal = false;
+  }
+  else {
+    motor = charToIndex(theByte)
+    readyForVal = true;
+  }
+}
+
+int charToIndex(char digit) {
+  switch (digit) {
+    case 's': return 0;
+    case 'w': return 1;
+    case 'h': return 2;
+    case 'y': return 3;
+    default: return -1;
+  }
+}
