43ffbb76f575f1e9edced902b8200789a1020646
zlevitt <zlevitt@users.noreply.github.com>
2016-04-15 08:45:37 -0400

    Added files via upload

---
+++ Combined_Driving_Code.ino
@@ -0,0 +1,73 @@
+# include <Servo.h>
+//Serial to motor
+//Input: joystick values as serial
+//Output: value sent to motor
+
+//CONSTANTS!!!
+const int numForces = 4;
+const int numberOfMotors = 3;
+const int motorPolarities[numberOfMotors] = {1, 1, 1}; // 1 for foreward, -1 for reverse
+const int maxMotorOutput = 1900;
+const int minMotorOutput = 1100;
+
+//END OF CONSTANTS
+
+boolean readyForVal = false;
+int joystickForces[numForces]; //array of joystick desired values received
+int joystickForceIndex; //stores the joystick number
+// {heave, surge, yaw} or {up, foreward, turn right}
+int motorOutputs[numberOfMotors]; // Output
+
+Servo servos[numberOfMotors];
+byte servoPins[numberOfMotors] = {1,2,3}; //Assign Pins Here
+
+
+void setup(){
+  Serial.begin(9600);
+
+    for(int i = 0; i < numberOfMotors; i++) {
+    servos[i].attach(servoPins[i]);
+    servos[i].writeMicroseconds(1500);
+  }
+  delay(1000); // delay to allow the ESC to recognize the stopped signal
+}
+
+void loop() {
+  int incomingByte; //to store byte
+
+  if(Serial.available()>0)
+  {
+    incomingByte = Serial.read();
+    evaluateByte(incomingByte); //sends to function to read serial value
+  }
+
+        motorOutputs[0] = motorPolarities[0] * joystickForces[0]; // Vertical Motor
+        motorOutputs[1] = motorPolarities[1] * (joystickForces[1] + joystickForces[2]); // Left Side Motor
+        motorOutputs[2] = motorPolarities[2] * (joystickForces[1] - joystickForces[2]); // Right Side Motor
+          for (int i = 0; i < numberOfMotors; i++) {
+          motorOutputs[i] = constrain(map(motorOutputs[i], -100, 100, minMotorOutput, maxMotorOutput), minMotorOutput, maxMotorOutput);
+
+            for(int i = 0; i < numberOfMotors; i++) {
+    servos[i].writeMicroseconds(motorOutputs[i]);
+  }
+}
+void evaluateByte(int theByte) {
+  if (readyForVal) {
+    joystickForces[joystickForceIndex] = map(theByte, 0, 127, -1.0, 1.0); //change the value to between -1 and 1 and put it in the correct spot in the array.
+    readyForVal = false;
+  }
+  else {
+    joystickForceIndex = charToIndex(theByte);
+    readyForVal = true;
+  }
+}
+
+int charToIndex(char digit) { // gives value corresponding to a motor. This is used to update the spot corresponding to that motor.
+  switch (digit) {
+    case 's': return 0;
+    case 'w': return 1;
+    case 'h': return 2;
+    case 'y': return 3;
+    default: return -1;
+  }
+}
